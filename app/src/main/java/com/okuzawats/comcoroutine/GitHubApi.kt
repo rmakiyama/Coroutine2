d30b0f27531d1dd2206f5b18f0f2de37e8eabceb
okuzawats <okuzawats@gmail.com>
2019-04-28 22:31:40 +0900

    Moshiへの依存を削除

--- app/src/main/java/com/okuzawats/comcoroutine/GitHubApi.kt
+++ app/src/main/java/com/okuzawats/comcoroutine/GitHubApi.kt
@@ -4,7 +4,6 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
